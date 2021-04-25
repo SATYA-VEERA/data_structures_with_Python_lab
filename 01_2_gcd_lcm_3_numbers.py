4b47a87ce640f0076d15413a27fbdd073ceaddd8
VEERA SATYA PAVAN VIGNESH <72861601+SATYA-VEERA@users.noreply.github.com>
2021-04-25 17:15:57 +0530

    Update 01_2_gcd_lcm_3_numbers.py

--- 01_2_gcd_lcm_3_numbers.py
+++ 01_2_gcd_lcm_3_numbers.py
@@ -1,3 +1,17 @@
 """ Python Script to find GCD and LCM of 3 numbers """
 
 # Write your code from here
+import math
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+gcd_xyz = math.gcd(math.gcd(x, y), z)
+gcd_xy = math.gcd(x, y)
+gcd_xz = math.gcd(x, z)
+gcd_yz = math.gcd(y, z)
+
+lcm_xyz = int((x*y*z*gcd_xyz)/(gcd_xy*gcd_yz*gcd_xz))
+
+print("gcd and lcm of ",x, ",", y, ",", z, " are ", gcd_xyz, ",", lcm_xyz, "respectively")
